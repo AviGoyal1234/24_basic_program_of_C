a782d03c917cd39885ffc91b15dd43bb8a961ffb
AviGoyal1234 <83627865+AviGoyal1234@users.noreply.github.com>
2021-05-26 21:10:01 +0530

    Update 24-seperation_of_integer_and_decimal_value.c

--- 24-seperation_of_integer_and_decimal_value.c
+++ 24-seperation_of_integer_and_decimal_value.c
@@ -3,9 +3,11 @@ int main()
 {
 	float a,b;
 	int c;
-	printf("Enter the number: ");
+	printf("Enter the number : ");
 	scanf("%f",&a);
-	c= (int)a;
-	b=a-c;
-	printf("Integer value = %d\nDecimal value = %f",c,b);
+	c = (int)a;
+	b = a - c;
+	printf("Integer value = %d\nDecimal value = %f", c,b);
+	
+	return 0;
 }
